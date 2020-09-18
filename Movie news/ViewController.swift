7c9260be01502d3c419347a6aa77f6113796c773
Kazybek <kzhapparkulov@gmail.com>
2020-09-18 09:38:27 +0600

    Initial Commit

---
+++ Movie news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie news
+//
+//  Created by Beka Zhapparkulov on 9/18/20.
+//  Copyright © 2020 Kazybek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
